145f15c8e1f4ad0ebf74d24d87d04bb41600630f
최은철 <wndqhrgkdlel@gmail.com>
2024-01-31 14:28:04 +0900

    내용 변경 후 파일지문 체크

--- fingerprint.js
+++ fingerprint.js
@@ -25,3 +25,11 @@ function fileFingerprint(filePath) {
 fileFingerprint('./example.txt')
     .then(fingerprint => console.log('File fingerprint:', fingerprint))
     .catch(err => console.error('Error:', err));
+
+// example.txt : file fingerprint를 테스트하기 위한 예시 파일입니다.
+// File fingerprint: 837fda7c8870b9add8b3e4feaeb495031b22e1b33a6f8f26c68edfd3b99021c9
+
+// example.txt의 내용을 바꿈
+
+// file fingerprint를 테스트하기 위한 예시 파일입니다. 기존 파일의 내용이 변경되면?
+// File fingerprint: 85e566343d7cc7a89f23d5b02be7ca80ca006bf7b4ccf346d8c928fd860a4f14
