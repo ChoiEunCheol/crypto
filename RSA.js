fd683bbe15d4541559ac0d1400bdf9a5931fc145
최은철 <wndqhrgkdlel@gmail.com>
2024-01-31 14:36:10 +0900

    복호화 과정

--- RSA.js
+++ RSA.js
@@ -26,3 +26,20 @@ const data = "암호화할 데이터";
 const encrypted = encryptData(data, publicKey);
 console.log("Encrypted Data:", encrypted);
 //Encrypted Data: F7kDU2S6mZoZlOUfPmRCbT+ZD9bDXbo2O//SM8cJzKix1zrOmUGYj7exLUWZyAB4HbGTlPsEHLpdZp8SjqmVoJdTLoZeEYBJt2M0PY+Indcw31c0qnXpIefpLeBS/F7Q8CWpHOkFnlHt6NwMokp8vYOvVCV9jOsH70JXW3UNClpVr6y13cp3NFMpnhuFSluPOwP9n77e8tplLDuCJqZPvJXNE94YBG49+MUQxCXaBlLJYbzbDMqB31oVrC+p6QRnyHzRzUMntJ+51zd9cU/6jWHNSbM95aowTFUxEFQes6r7tWSYn6HubY8JcMXT1gFKCUnCCBHOXgbd/bmZg62pyg==
+
+function decryptData(encryptedData, privateKey) {
+    const decryptedData = crypto.privateDecrypt(
+        {
+            key: privateKey,
+            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+            oaepHash: "sha256",
+        },
+        Buffer.from(encryptedData, "base64")
+    );
+
+    return decryptedData.toString();
+}
+
+const decrypted = decryptData(encrypted, privateKey);
+console.log("Decrypted Data:", decrypted);
+//Decrypted Data: 암호화할 데이터
